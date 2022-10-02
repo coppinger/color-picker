9ed663e16e9aafe58dcc54a866f9d08be2bfaba8
Charlie Coppinger <charles.coppinger@gmail.com>
2022-10-02 17:53:03 +1300

    done core requirements

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 let pickedColor = "0047AB";
+let pickedMode = "monochrome";
+
 let colorPicker = document.querySelector("input");
+let modePicker = document.querySelector("select");
+
+function updatePickedColor(event) {
+  pickedColor = event.target.value.slice(1);
+}
+
+function updatePickedMode(event) {
+  pickedMode = event.target.value;
+}
+
+function setPickedColor() {
+  getColors();
+  colorPicker.addEventListener("input", updatePickedColor, false);
+  modePicker.addEventListener("input", updatePickedMode, false);
+}
+
+window.addEventListener("load", setPickedColor, false);
 
 let colorP = document.querySelectorAll(".color-p");
 
 function getColors() {
-  fetch(`https://www.thecolorapi.com/scheme?hex=${pickedColor}`)
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${pickedColor}&mode=${pickedMode}`
+  )
     .then((res) => res.json())
     .then((json) => {
       for (let i = 0; i < json.colors.length; i++) {
